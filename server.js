599e3e100be52d78808b6fb30d8e1f36b0eda386
Logan Bias <loganbiasomma@gmail.com>
2016-08-07 00:06:08 +0600

    ip info fix

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
 
+
+var getFirstNumPos = function(d){
+  var da = d.split('');
+  for (var i=0;i<da.length;i++){
+    if (!isNaN(da[i])){
+      return i;
+    }
+  }
+  return 0;
+}
 app.get('/', function (req, res) {
   var ob = {
-    "ipaddress":req.ip,
+    "ipaddress":req.ip.substring(getFirstNumPos(req.ip)),
     "language":req.get('Accept-Language').substring(0,req.get('Accept-Language').indexOf(',')),
     "software":req.get('user-agent').substring(req.get('user-agent').indexOf('(')+1,req.get('user-agent').indexOf(')'))
   }
